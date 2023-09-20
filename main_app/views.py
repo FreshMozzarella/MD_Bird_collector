952f4d7f5f16be3c80a9cdd57df26c7e9c096a96
Alec <106216445+FreshMozzarella@users.noreply.github.com>
2023-09-20 16:02:55 -0400

    fixed access token

--- main_app/views.py
+++ main_app/views.py
@@ -22,27 +22,42 @@ def birds_index(request):
     try:
         # Fetch bird data from Xeno-Canto
         recordings = fetch_bird_calls_from_xenocanto()
-       
     except Exception as e:
         # Log the exception for debugging purposes
         print(f"Error fetching from Xeno-Canto: {e}")
         return render(request, 'error_template.html', {
             'message': 'Failed to fetch data from Xeno-Canto'
         })
-    limited_recordings = recordings[:10]
+
+    # Set to keep track of species we've seen
+    seen_species = set()
+
+    # List to store unique species recordings
+    unique_recordings = []
+
     # Assuming you have some way to get the iNaturalist access token:
     access_token = "eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjo3NDI3ODcxLCJleHAiOjE2OTUzMTg2Mjh9.LPHWZImGCvO9SExggffDtbhmeo87MxOY2Y2_XaoWeZdT8XdTHk0A87YNtmfHoPegbgYURKeCdd4ar2QPnRlLlA"
 
-    for recording in limited_recordings:
-        try:
-            species_name = recording['gen'] + " " + recording['sp']
-            recording['image_url'] = fetch_image_from_inaturalist(species_name, access_token)
-        except Exception as e:
-            # Log the exception for debugging purposes
-            print(f"Error fetching image from iNaturalist for {species_name}: {e}")
-            recording['image_url'] = None  # Default to no image if there's an error
+    for recording in recordings:
+        species_name = recording['gen'] + " " + recording['sp']
+
+        # If the species is unique and hasn't been processed yet
+        if species_name not in seen_species:
+            try:
+                recording['image_url'] = fetch_image_from_inaturalist(species_name, access_token)
+                unique_recordings.append(recording)
+                seen_species.add(species_name)
+            except Exception as e:
+                # Log the exception for debugging purposes
+                print(f"Error fetching image from iNaturalist for {species_name}: {e}")
+                recording['image_url'] = None  # Default to no image if there's an error
+        
+        # Stop once we've processed 10 unique species
+        if len(unique_recordings) >= 10:
+            break
+
     return render(request, 'birds/index.html', {
-        'recordings': limited_recordings
+        'recordings': unique_recordings
     })
 
 
